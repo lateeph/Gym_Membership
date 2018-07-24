b9445a570bfe8a240af830421578907e8f6c163b
Abdul-Latif Sumani <latifelnino@yahoo.com>
2018-07-24 21:57:03 +0000

    login

--- controllers/default.py
+++ controllers/default.py
@@ -5,5 +5,35 @@ def home():
 def register():
 	return dict()
 
+def store():
+	submitted_firstname = request.vars.firstname
+	submitted_lastname = request.vars.lastname
+	submitted_phonenumber = request.vars.phonenumber
+	submitted_gender = request.vars.gender
+	submitted_email = request.vars.email
+	submitted_password = request.vars.password
+
+	results = db.users.insert(
+		db_firstname=submitted_firstname,
+		db_lastname=submitted_lastname,
+		db_phonenumber=submitted_phonenumber,
+		db_gender=submitted_gender,
+		db_email=submitted_email,
+		db_password=submitted_password
+	)
+
+	if results:
+		return "User Saved Successfully"
+	else:
+		return "An Error Occured"
+
+def seeUsers():
+	users =db().select(db.users.ALL)
+	return dict(users=users)
+
+
 def login():
+	return dict()
+
+def qqq():
 	return dict()
